e446d33770f4c91d26830328bb261242c77117e0
aws.student1911@gmail.com <aws.student1911@gmail.com>
2025-02-13 23:33:03 +0200

    updated for dynamodb utilizaton

---
+++ Controllers/MysfitsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ModernWebAppNET.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MysfitsController : ControllerBase
+    {
+        private readonly MysfitsService _mysfitsService;
+        public MysfitsController(MysfitsService mysfitsService)
+        {
+            _mysfitsService = mysfitsService;
+        }
+        // GET api/mysfits
+        [HttpGet]
+        public async Task<ActionResult<List<Mysfit>>> Get([FromQuery]FilterRequest filter)
+        {
+            var mysfits = new List<Mysfit>();
+            if (String.IsNullOrEmpty(filter.filter) && String.IsNullOrEmpty(filter.value))
+            {
+                mysfits = await _mysfitsService.GetMysfits();
+            }
+            else
+            {
+                mysfits = await _mysfitsService.GetMysfitsWithFilter(filter);
+            }
+            return new JsonResult(new { mysfits = mysfits });
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,14 +1,24 @@
+using Amazon.DynamoDBv2;
+using ModernWebAppNET;
+using ModernWebAppNET.Utilities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDefaultAWSOptions(builder.Configuration.GetAWSOptions()); //options are in appsettings under AWS
+builder.Services.AddAWSService<IAmazonDynamoDB>();
+builder.Services.AddTransient<MysfitsService>();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+
+app.UseMiddleware<ErrorHandlingMiddleware>();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
